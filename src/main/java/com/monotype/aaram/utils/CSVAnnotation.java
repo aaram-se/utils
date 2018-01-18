5f818e135a24751d88b84fe5c7b399d19535a5da
kohli-harshit <harshit.kohli@monotype.com>
2018-01-18 19:42:27 +0530

    adding first version

---
+++ src/main/java/com/monotype/aaram/utils/CSVAnnotation.java
@@ -0,0 +1,21 @@
+package com.monotype.aaram.utils;
+
+import java.lang.annotation.*;
+
+/**
+ * Class for storing Custom annotations related to CSV Manipulation
+ */
+public class CSVAnnotation {
+
+	/**
+	 * This is a Custom Annotation which provides the facility to provide a CSV File Path to the com.monotype.aaram.utils.GenericDataProvider Class
+	 * In order to use it, we need to add the following annotation to the test case declaration :-
+	 * @CSVFileParameters(path = "test-data\\api\\cards\\csv\\CardsPostApiTestData.csv", delimiter = "###")
+	 */
+	@Target(ElementType.METHOD)
+	@Retention(RetentionPolicy.RUNTIME)
+	public @interface CSVFileParameters {
+		String path();
+		String delimiter();
+	}
+}
